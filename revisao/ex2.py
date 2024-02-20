9bd6017f3766990100bd90072ee854f2375cb12e
Leonardo Cutrim <141735210+yCarpal@users.noreply.github.com>
2024-02-20 14:47:00 +0000

    finalizar os 3 restantes

---
+++ revisao/ex2.py
@@ -0,0 +1,14 @@
+tamnho_vetor = []
+Vetor = []
+Vetor_sem_zeros= []
+
+for i in range(15):
+    Vetor.append(int(input("Insira um valor para o vetor: ")))
+    if Vetor[i] > 0:
+        Vetor_sem_zeros.append(Vetor[i])
+
+print(Vetor_sem_zeros)
+
+                 
+        
+        
