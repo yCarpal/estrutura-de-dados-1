db1ba4060611dfe797d4f437f848e68b7675b9fd
Leonardo Cutrim <141735210+yCarpal@users.noreply.github.com>
2024-03-05 14:36:23 +0000

    exercicios listas lineares

---
+++ lsitas_lineares/ex1.py
@@ -0,0 +1,8 @@
+lista = []
+def inserir(valor):
+
+    lista.append(int(input("Insira algum elemento na lista:")))
+    list.insert(K,(int(input("Insira um elemento: "))))
+               #K = igual a qualquer i da lista
+
+def remover 
