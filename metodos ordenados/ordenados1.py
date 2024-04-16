ae88191f6a2920fd9b2fc5f2165b26a77f1815c8
Leonardo Cutrim <141735210+yCarpal@users.noreply.github.com>
2024-04-16 12:56:40 +0000

    att

--- metodos ordenados/ordenados1.py
+++ metodos ordenados/ordenados1.py
@@ -1,18 +1,18 @@
 def bubble_sort(arr):
     n = len(arr)
-    for i in range(n):
+    for i in range(n)-1:
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
 def selection_sort(arr):
     n = len(arr)
-    for i in range(n):
-        min_idx = i
-        for j in range(i+1, n):
+    for step in range(n):
+        min_idx = step
+        for j in range(step + 1, n):
             if arr[j] < arr[min_idx]:
                 min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        arr[step], arr[min_idx] = arr[min_idx], arr[step]
 
 def insertion_sort(arr):
     n = len(arr)
